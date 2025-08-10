128d73d2f6caa3ce8463c7c3d4014363be959a3d
vmosharova <varya.mosharova@gmail.com>
2025-08-10 19:40:31 +0200

    Fix: base path for gh-pages

--- src/index.js
+++ src/index.js
@@ -23,24 +23,24 @@ import { Toaster } from "react-hot-toast";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <ScrollToTop>
-      <Provider store={store}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product" element={<Products />} />
-          <Route path="/product/:id" element={<Product />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="*" element={<PageNotFound />} />
-          <Route path="/product/*" element={<PageNotFound />} />
-        </Routes>
-      </Provider>
-    </ScrollToTop>
-    <Toaster />
-  </BrowserRouter>
+<BrowserRouter basename={process.env.PUBLIC_URL}>
+  <ScrollToTop>
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Products />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="*" element={<PageNotFound />} />
+        <Route path="/product/*" element={<PageNotFound />} />
+      </Routes>
+    </Provider>
+  </ScrollToTop>
+  <Toaster />
+</BrowserRouter>
 );
